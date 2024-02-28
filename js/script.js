a2fb19d9247c1cca9c8d88a6815ac0c5c4af6edb
infntyrepeating <infntyrepeating@gmail.com>
2024-02-28 00:51:54 +0000

    Adding some content!

---
+++ js/script.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+  Particles.init({
+    selector: '.background',
+    maxParticle: 150,
+    speed:1,
+    minDistance: 80,
+    sizeVariaton: 1,
+    color: '#d6ffdd'
+  })
+}
